ef024e043e233908df15116b4707f5733f1ca20f
Raven Ampere <164369957+ampere07@users.noreply.github.com>
2026-05-17 14:43:58 +0800

    Remove unused imports from Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import { Network, ShieldCheck, Zap } from 'lucide-react';
 import { Link } from 'react-router-dom';
 import { PANEL_IDS } from '../constants';
 
